fc657b3a23b7385dc837789610bce11c4f6c73f2
Hayden <hmorran@outlook.com>
2019-04-10 11:32:18 +1000

    added basic routing functions to app

--- src/App/Layout/App.js
+++ src/App/Layout/App.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
 import { Container } from 'semantic-ui-react';
+import { Route, Switch } from 'react-router-dom';
 import EventDashboard from '../../App/Features/Event/EventDashboard/EventDashboard';
 import NavBar from '../Features/Nav/NavBar/NavBar';
+import HomePage from '../Features/Home/HomePage';
+import EventForm from '../Features/Event/EventForm/EventForm';
+import SettingsDashboard from '../Features/User/Settings/SettingsDashboard';
+import UserDetailedPage from '../Features/User/UserDetailed/UserDetailedPage';
+import PeopleDashboard from '../Features/User/PeopleDashboard/PeopleDashboard';
+import EventDetailedPage from '../Features/Event/EventDetailed/EventDetailedPage';
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        <NavBar />
-        <Container className="main">        
-          <EventDashboard />
-        </Container>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+        </Switch>
+        <Route path='/(.+)' render={() => (
+        <div>
+          <NavBar />
+          <Container className="main">
+            <Switch>
+              <Route path='/events' component={EventDashboard} />
+              <Route path='/events/:id' component={EventDetailedPage} />
+              <Route path='/people' component={PeopleDashboard} />
+              <Route path='/profile/:id' component={UserDetailedPage} />
+              <Route path='/settings' component={SettingsDashboard} />
+              <Route path='/createEvent' component={EventForm} />
+            </Switch>
+          </Container>
+        </div>
+        )} />
       </div>      
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css'
 import './index.css';
 import App from './App/Layout/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>    
+
+                , document.getElementById('root'));
 
 serviceWorker.unregister();
